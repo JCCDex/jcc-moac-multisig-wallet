34cba6ec8892afda391883deefff13bf3cb6d2ae
GinMu <gin.musheng@gmail.com>
2019-10-08 21:26:10 +0800

    unit test for util.ts

---
+++ dapp/test/unit/specs/js/util.spec.js
@@ -0,0 +1,42 @@
+import * as util from "@/js/util";
+
+describe("test util.ts", () => {
+
+  describe("test isValidNumber", () => {
+    test("return true if the input value is valid number", () => {
+      expect(util.isValidNumber(0)).toBe(true)
+      expect(util.isValidNumber(0.1111)).toBe(true);
+    })
+
+    test("return false if the input value isn't valid number", () => {
+      const values = [null, undefined, {}, -1, Infinity, NaN, "1"];
+      for (const value of values) {
+        expect(util.isValidNumber(value)).toBe(false);
+      }
+    })
+  })
+
+  describe("test isDev", () => {
+    test("return true if the NODE_ENV is 'development'", () => {
+      process.env.NODE_ENV = "development";
+      expect(util.isDev()).toBe(true);
+    })
+
+    test("return false if the NODE_ENV isn't 'development'", () => {
+      process.env.NODE_ENV = "production";
+      expect(util.isDev()).toBe(false);
+    })
+  })
+
+  describe("test isMainnet", () => {
+    test("return true if the MAINNET is 'true'", () => {
+      process.env.MAINNET = "true";
+      expect(util.isMainnet()).toBe(true);
+    })
+
+    test("return false if the MAINNET is 'false'", () => {
+      process.env.MAINNET = "false";
+      expect(util.isMainnet()).toBe(false);
+    })
+  })
+})
