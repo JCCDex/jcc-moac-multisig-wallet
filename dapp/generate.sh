23bac70190cc1312d84edc384ad42e21de630f91
GinMu <gin.musheng@gmail.com>
2019-10-09 18:19:42 +0800

    update dev.sh & generate.sh

--- dapp/generate.sh
+++ dapp/generate.sh
@@ -2,4 +2,5 @@
 
 cross-env Contract=$1 Node=$2 Mainnet=$3 nuxt-ts generate
 
-# npm run generate "0x5ca7bae311d4092d23b2854e895548d451dcae5c" http://192.168.66.249:8546 false
+# test node
+# npm run generate "0x8eca41a83ea0efbd41401ed850774974bda6b697" http://106.12.39.168:8546 false
