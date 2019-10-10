78925bc044b168384885658ebaaa98dedd7d639f
GinMu <gin.musheng@gmail.com>
2019-10-10 11:50:23 +0800

    update public test node

--- dapp/generate.sh
+++ dapp/generate.sh
@@ -3,4 +3,4 @@
 cross-env Contract=$1 Node=$2 Mainnet=$3 nuxt-ts generate
 
 # test node
-# npm run generate "0x8eca41a83ea0efbd41401ed850774974bda6b697" http://106.12.39.168:8546 false
+# npm run generate "0x8eca41a83ea0efbd41401ed850774974bda6b697" https://mtnode1.jccdex.cn false
