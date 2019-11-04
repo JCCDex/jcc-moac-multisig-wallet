e5d65eb7b33b0ba2f90bab239c66fc63169308b2
GinMu <gin.musheng@gmail.com>
2019-11-04 12:38:35 +0800

    update contract address

--- dapp/generate.sh
+++ dapp/generate.sh
@@ -3,4 +3,4 @@
 cross-env Contract=$1 Node=$2 Mainnet=$3 nuxt-ts generate
 
 # test node
-# npm run generate "0x98a3643e860f7f782194dd3e80408f90d4e423ae" https://mtnode1.jccdex.cn false
+# npm run generate "0x959fe8e1b07ce9b376f8ab76de6bdfae78c25e3a" https://mtnode1.jccdex.cn false
