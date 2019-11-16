e9f6bb482247cf91ffae8599d16f8e19a1c4d7fd
GinMu <gin.musheng@gmail.com>
2019-11-16 21:11:45 +0800

    update generate.sh

--- dapp/generate.sh
+++ dapp/generate.sh
@@ -4,3 +4,7 @@ cross-env Contract=$1 Node=$2 Mainnet=$3 nuxt-ts generate
 
 # test node
 # npm run generate "0xf08e4103ab71564dffe092fe4472bae43847b48c" https://mtnode1.jccdex.cn false
+
+
+# main node
+# npm run generate "0x23d5505451833d70d74d47834ed415329c8a0e96" https://chain3.mytokenpocket.vip true
